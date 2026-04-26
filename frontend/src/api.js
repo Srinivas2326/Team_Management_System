cc3d197bb8926a95ca6c49eb08e55b76f9bac3f1
srinivas <srinivas23peddada@gmail.com>
2026-04-26 20:29:36 +0530

    backend deployed link added

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "https://team-management-system-ku5a.onrender.com"
 });
 
 export default API;
