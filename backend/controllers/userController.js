a86221a91597f75bfab339b1b198accf9ec31974
srinivas <srinivas23peddada@gmail.com>
2026-04-26 20:40:26 +0530

    userController.js updated

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,10 +2,32 @@ const User = require("../models/User");
 
 exports.createUser = async (req, res) => {
   try {
-    const user = await User.create(req.body);
+    const { name, email } = req.body;
+
+    if (!name || !email) {
+      return res.status(400).json({
+        message: "Name and Email required"
+      });
+    }
+
+    const existing = await User.findOne({ email });
+
+    if (existing) {
+      return res.status(400).json({
+        message: "Email already exists"
+      });
+    }
+
+    const user = await User.create({ name, email });
+
     res.status(201).json(user);
+
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    console.log("Create User Error:", error);
+
+    res.status(500).json({
+      message: error.message
+    });
   }
 };
 
@@ -14,6 +36,8 @@ exports.getUsers = async (req, res) => {
     const users = await User.find();
     res.json(users);
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    res.status(500).json({
+      message: error.message
+    });
   }
 };
